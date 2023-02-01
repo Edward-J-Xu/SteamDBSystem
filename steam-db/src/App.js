6e831c9dd3705d41f5753c94dbc01d9652ff6045
Edward-J-Xu <1614257233@qq.com>
2023-02-01 10:21:17 -0500

    React Router Dom

--- steam-db/src/App.js
+++ steam-db/src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Router>
+        <Routes>
+          <Route index element={<h1>Hello World 2</h1>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
