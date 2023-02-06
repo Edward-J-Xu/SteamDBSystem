12099ba5e3c7ecd39441618afed5d402749cb46b
Edward-J-Xu <1614257233@qq.com>
2023-02-05 23:04:46 -0500

    Draft for APIs

---
+++ steam-db/src/components/header/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.css"
+import { Link } from "react-router-dom"
+
+const Header = () => {
+    return (
+        <div className="header">
+            <div className="headerLeft">
+                <Link to="/"><img className="header_icon" src="https://cdn-icons-png.flaticon.com/512/220/220223.png" /></Link>
+                <Link to="/games/popular" style={{textDecoration: "none"}}><span>Popular</span></Link>
+                <Link to="/games/top_rated" style={{textDecoration: "none"}}><span>Top Rated</span></Link>
+                <Link to="/games/upcoming" style={{textDecoration: "none"}}><span>Upcoming</span></Link>
+            </div>
+        </div>
+    )
+}
+
+export default Header
