f5b829b172cd3bbf15586caff6200a778b7b37ef
Edward-J-Xu <1614257233@qq.com>
2023-02-22 22:26:47 -0500

    Basic Post Router and connecting to local db

---
+++ server/models/Posts.js
@@ -0,0 +1,26 @@
+
+// Creating Posts Table
+/*
+Executing (default): SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_NAME = 'Posts' AND TABLE_SCHEMA = 'steamdb'
+Executing (default): CREATE TABLE IF NOT EXISTS `Posts` (`id` INTEGER NOT NULL auto_increment , `title` VARCHAR(255) NOT NULL, `postText` VARCHAR(255) NOT NULL, `username` VARCHAR(255) NOT NULL, `createdAt` DATETIME NOT NULL, `updatedAt` DATETIME NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB;
+Executing (default): SHOW INDEX FROM `Posts` FROM `steamdb`
+*/
+
+module.exports = (sequelize, DataTypes) => {
+    const Posts = sequelize.define("Posts", {
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      postText: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    });
+  
+    return Posts;
+  };
