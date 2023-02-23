b02ef4c57b9fc5510e1089a352bc22948fdbf947
Edward-J-Xu <1614257233@qq.com>
2023-02-23 18:07:06 -0500

    Create New Post and Individual Post Page

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,7 @@ import GameList from './components/gameList/gameList';
 import Game from './pages/gameDetail/game';
 import PostApp from './pages/posts/postApp';
 import CreatePost from "./pages/posts/createPost";
+import Post from "./pages/posts/Post";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
                 <Route path="game/:id" element={<Game />}></Route>
                 <Route path="games/:type" element={<GameList />}></Route>
                 <Route path="games/posts"  element={<PostApp />}></Route>
+                <Route path="games/posts/:id"  element={<Post />}></Route>
                 <Route path="games/posts/createpost"  element={<CreatePost />}></Route>
                 <Route path="/*" element={<h1>Error Page</h1>}></Route>
             </Routes>
