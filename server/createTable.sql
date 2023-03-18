438bcf6bfac3433046b88d59552e69295cfd64e4
KevinJie <sjie@uwaterloo.ca>
2023-03-18 17:09:19 -0400

    Import game dataset into database

--- server/createTable.sql
+++ server/createTable.sql
@@ -32,3 +32,19 @@ create table if not exists likes
     constraint   like_post_id foreign key (post_id) references posts (id) on delete cascade,
     constraint   like_user_id foreign key (user_id) references users (id) on delete cascade
 );
+
+create table if not exists game
+(
+    game_id int primary key,
+    name varchar(100) not null,
+    genre varchar(50) not null,
+    language varchar(50) not null,
+    platform varchar(10) not null,
+    developer varchar(50) not null,
+    release_year int not null,
+    release_month int not null,
+    release_day int not null,
+    rating decimal(3,1),
+    current_price decimal(10, 2) not null,
+    game_image varchar(200) not null
+);
