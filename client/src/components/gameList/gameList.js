0f6c5d381efe7f0a250e946f793aff243f401de6
Bob Liu <liukezhen@Bobs-MacBook-Pro.local>
2023-03-26 21:08:27 -0400

    gamelist and game

--- client/src/components/gameList/gameList.js
+++ client/src/components/gameList/gameList.js
@@ -1,39 +1,48 @@
-import React, {useEffect, useState} from "react"
-import "./gameList.css"
-import { useParams } from "react-router-dom"
-import Cards from "../card/card"
+import React, { useEffect, useState } from "react";
+import "./gameList.css";
+import { useParams } from "react-router-dom";
+import Cards from "../card/card";
+import Papa from "papaparse";
+import gameDataCSV from "../../database/gamedb.csv";
 
 const GameList = () => {
-    
-    const [gameList, setGameList] = useState([])
-    const {type} = useParams()
+  const [gameList, setGameList] = useState([]);
+  const { type } = useParams();
 
-    useEffect(() => {
-        getData()
-    }, [])
-
-    useEffect(() => {
-        getData()
-    }, [type])
-
+  useEffect(() => {
     const getData = () => {
-        fetch(`https://api.themoviedb.org/3/movie/${type ? type : "popular"}?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`)
-        .then(res => res.json())
-        .then(data => setGameList(data.results))
-    }
+      Papa.parse(gameDataCSV, {
+        header: true,
+        download: true,
+        dynamicTyping: true,
+        complete: (results) => {
+          let data = results.data;
+          if (type) {
+            if (type.toLowerCase() === "popular" || type.toLowerCase() === "top_rated") {
+              data.sort((a, b) => b.rating - a.rating);
+              data = data.slice(0, 50); // Show top 50 games
+            } else {
+              data = data.filter((game) => game.genre.toLowerCase() === type.toLowerCase());
+            }
+          }
+          setGameList(data);
+        },
+      });
+    };
 
-    return (
-        <div className="game__list">
-            <h2 className="list__title">{(type ? type : "POPULAR").toUpperCase()}</h2>
-            <div className="list__cards">
-                {
-                    gameList.map(game => (
-                        <Cards game={game} />
-                    ))
-                }
-            </div>
-        </div>
-    )
-}
+    getData();
+  }, [type]);
 
-export default GameList
+  return (
+    <div className="game__list">
+      <h2 className="list__title">{(type ? type : "ALL").toUpperCase()}</h2>
+      <div className="list__cards">
+        {gameList.map((game) => (
+          <Cards key={game.game_id} game={game} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default GameList;
